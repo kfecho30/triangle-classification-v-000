ca800e0aea003a49816edf2d4b3a80aaffff051c
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 19:54:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,11 @@ class Triangle
   end
 
   def kind
-    real_tri
-    if @s1 == @s2 && @s1 == @s3
+    if @s1 <= 0 || @s2 <= 0 || @s3 <= 0
+      raise TriangleError
+    elsif @s1+@s2 < @s3 || @s2+@s3 < @s1 ||  @s1+@s3 < @s2
+      raise TriangleError
+    elsif @s1 == @s2 && @s1 == @s3
       :equilateral
     elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
       :isosceles
